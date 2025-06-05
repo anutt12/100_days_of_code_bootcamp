c40311428861c349550f8a111831cff6dbe978e5
Andrew Nutt <87488155+anutt12@users.noreply.github.com>
2025-06-04 19:19:18 -0700

    Band Name Generator

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,3 +1,4 @@
-name = "Andrew"
-length = len(name)
-print(length)
+print("Welcome to the Band Name Generator!")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
