6a1b7b5512313f6f4352edb811a063800bdf55d5
Andrew Nutt <87488155+anutt12@users.noreply.github.com>
2025-06-16 17:53:58 -0700

    Index Errors and Nested Lists

---
+++ Day 4/IndexError/task.py
@@ -0,0 +1,5 @@
+fruits = ['strawberries', 'apples', 'grapes', 'pears', 'nectarines', 'peaches', 'cherries']
+vegetables = ['spinach', 'kale', 'tomatoes', 'celery', 'potatoes']
+
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen)
