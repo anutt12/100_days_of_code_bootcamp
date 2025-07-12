7f722dab1db6168e8f60e01bac2beb9cc5fc70e0
Andrew Nutt <87488155+anutt12@users.noreply.github.com>
2025-07-12 14:20:34 -0700

    Started Step 4

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,4 +1,54 @@
 import random
+stages = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
 
 word_list = ["aardvark", "baboon", "camel"]
 
@@ -13,21 +63,32 @@ print(placeholder)
 
 game_over = False
 correct_letters = []
+lives = 6
 
 while not game_over:
     guess = input("Guess a letter: ").lower()
 
     display = ""
+    was_correct = False
 
     for letter in chosen_word:
         if letter == guess:
             display += letter
             correct_letters.append(guess)
+            was_correct = True
         elif letter in correct_letters:
             display += letter
         else:
             display += "_"
+
+    if not was_correct:
+        lives -= 1
+        if lives == 0:
+            print("You lose!")
+            game_over = True
+
     print(display)
+    print(stages[lives])
 
     if "_" not in display:
         print("You win!")
