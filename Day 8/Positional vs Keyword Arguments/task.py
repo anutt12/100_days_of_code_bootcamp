df19391b9ff28f7070a89ce7aadbbb8c37750e6f
Andrew Nutt <87488155+anutt12@users.noreply.github.com>
2025-08-24 09:08:40 -0700

    Positional vs Keyword Arguments

--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,18 @@
 # Functions with input
+#
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+#
+#
+# greet_with_name("Jack Bauer")
 
-def greet_with_name(name):
+# Functions with more than 1 input
+def greet_with(name, location):
     print(f"Hello {name}")
-    print(f"How do you do {name}?")
+    print(f"What is it like in {location}?")
 
 
-greet_with_name("Jack Bauer")
+# greet_with("Jack Bauer", "Nowhere")
+greet_with(location="Nowhere", name="Jack Bauer")
+
