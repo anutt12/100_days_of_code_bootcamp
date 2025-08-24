a1babc5dfbd48e78e0b775b74fefa45dd835f4e2
Andrew Nutt <87488155+anutt12@users.noreply.github.com>
2025-08-24 10:20:31 -0700

    Caesar Cipher 2

--- Day 8/Caesar Cipher 2/main.py
+++ Day 8/Caesar Cipher 2/main.py
@@ -1,4 +1,5 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
@@ -11,16 +12,19 @@ shift = int(input("Type the shift number:\n"))
 # TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
 #  Use the value of the user chosen 'direction' variable to determine which functionality to use.
 
-def encrypt(original_text, shift_amount):
-    cipher_text = ""
+def caesar(original_text, shift_amount, direction_choice):
+    output_text = ""
+    if direction_choice == "decode":
+        shift_amount *= -1
     for letter in original_text:
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        cipher_text += alphabet[shifted_position]
-    print(f"Here is the encoded result: {cipher_text}")
+        if letter in alphabet:
+            index = alphabet.index(letter)
+            new_index = (index + shift_amount) % 26
+            output_text += alphabet[new_index]
+        else:
+            output_text += letter
+    print(f"{direction_choice}d text: {output_text}")
 
 
-encrypt(original_text=text, shift_amount=shift)
-
-
-
+if direction == "encode" or direction == "decode":
+    caesar(text, shift, direction)
