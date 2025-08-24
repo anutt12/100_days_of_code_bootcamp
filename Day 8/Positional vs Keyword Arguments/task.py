bca49e42fac7aafd08e31ce52ffcbeb52b506ba3
Andrew Nutt <87488155+anutt12@users.noreply.github.com>
2025-08-24 09:33:48 -0700

    Added Love Calculator coding exercise

--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -8,11 +8,28 @@
 # greet_with_name("Jack Bauer")
 
 # Functions with more than 1 input
-def greet_with(name, location):
-    print(f"Hello {name}")
-    print(f"What is it like in {location}?")
+# def greet_with(name, location):
+#     print(f"Hello {name}")
+#     print(f"What is it like in {location}?")
+#
+#
+# # greet_with("Jack Bauer", "Nowhere")
+# greet_with(location="Nowhere", name="Jack Bauer")
+
+def calculate_love_score(name1, name2):
+    keywords = ["true", "love"]
+
+    names = [n.replace(" ", "").lower() for n in [name1, name2]]
+
+    results = []
+    for word in keywords:
+        total = 0
+        for name in names:
+            for letter in name:
+                total += word.count(letter)
+        results.append(str(total))
+
+    print("".join(results))
 
 
-# greet_with("Jack Bauer", "Nowhere")
-greet_with(location="Nowhere", name="Jack Bauer")
-
+calculate_love_score("Angela Yu", "Jack Bauer")
